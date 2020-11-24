518026a0f419dedd751bc34b99c74aca8efda2ab
Inbal Tako <inbal@securenative.com>
2020-11-24 11:24:37 +0200

    Remove changelog from github actions

--- src/Utils.php
+++ src/Utils.php
@@ -5,7 +5,8 @@ namespace SecureNative\sdk;
 const ALGORITHM = "AES-256-CBC";
 const BLOCK_SIZE = 16;
 const AES_KEY_SIZE = 32;
-const IP_HEADERS = ["HTTP_X_FORWARDED_FOR", "X_FORWARDED_FOR", "HTTP_X_CLIENT_IP", "HTTP_X_REAL_IP", "REMOTE_ADDR", "x-forwarded-for", "x-client-ip", "x-real-ip", "x-forwarded", "x-cluster-client-ip", "forwarded-for", "forwarded", "via"];
+const IP_HEADERS = ["HTTP_X_FORWARDED_FOR", "X_FORWARDED_FOR", "HTTP_X_CLIENT_IP", "HTTP_X_REAL_IP", "HTTP_REMOTE_ADDR", "x-forwarded-for", "x-client-ip", "x-real-ip", "x-forwarded", "x-cluster-client-ip", "forwarded-for", "forwarded", "via"];
+const PII_HEADERS = ['HTTP_AUTHORIZATION', 'HTTP_ACCESS_TOKEN', 'HTTP_APIKEY', 'HTTP_PASSWORD',  'HTTP_PASSWD', 'HTTP_SECRET', 'HTTP_API_KEY'];
 
 abstract class Utils
 {
@@ -52,7 +53,7 @@ abstract class Utils
     {
         $headers = array();
         foreach ($_SERVER as $key => $val) {
-            if (substr($key, 0, 5) == 'HTTP_') {
+            if (substr($key, 0, 5) == 'HTTP_' && !in_array($key, PII_HEADERS) && !in_array(strtoupper($key), PII_HEADERS)) {
                 $name = strtolower(substr($key, 5));
                 $name = str_replace("_", "-", $name);
                 $headers[$name] = $val;
